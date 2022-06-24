837f1714d76fce996442837b9e978a08794010ca
JoaoFernandes02 <101251350+JoaoFernandes02@users.noreply.github.com>
2022-06-24 17:56:25 +0100

    Menu Update

--- HumanResources/src/humanresources/Main.java
+++ HumanResources/src/humanresources/Main.java
@@ -17,103 +17,204 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    public static boolean showEmpresas(ArrayList<Company> companies) {
+        if (companies.size() == 0) {
+            return false;
+        } else {
+            System.out.println("\nLista de Empresas: \n");
+            for (Company company : companies) {
+                System.out.println(company + "\n");
+            }
+            return true;
+        }
+    }
+
+    public static Company getEmpresa(ArrayList<Company> companies, String companyName) {
+        if (companies.size() == 0) {
+            return null;
+        } else {
+            for (Company company : companies) {
+                if (company.getName().equals(companyName)) {
+                    return company;
+                }
+            }
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         InputReader input = new InputReader();
-        Company company = new Company("IPS");
 
-        int option;
+        ArrayList<Company> companies = new ArrayList<>();
+
+        Company selectedCompany;
+
+        int option = 0;
+        int secOption = 0;
 
         do {
-            System.out.println("*** Gestão de Recursos Humanos ***");
-            System.out.println(company + "\n");
-            System.out.println("1. Adicionar Empregado");
-            System.out.println("2. Pesquisar Empregado");
-            System.out.println("3. Carregar Lista de Empregados");
-            System.out.println("4. Número de Empregados de uma Categoria");
-            System.out.println("5. Consultar Fichas de Empregados");
-            System.out.println("6. Consultar Fichas de Empregados, filtrados por categoria");
-            System.out.println("7. Total de Salários a Pagar");
-            System.out.println("8. Custos Trimestrais, Semestrais e Anuais com Salários");
-            System.out.println("9. Guardar Lista de Empregados");
-            System.out.println("10. Alterar Informações da Empresa");
+            System.out.println("\n*** Gestão de Recursos Humanos ***");
+            System.out.println("1. Ver Lista de Empresas");
+            System.out.println("2. Adicionar uma Empresa");
+            System.out.println("3. Remover uma Empresa");
+            System.out.println("4. Editar uma Empresa");
             System.out.println("0. Sair do Programa");
 
             option = input.getIntegerNumber("Escolha uma opção");
 
             switch (option) {
-                case 1 ->
-                    company.addEmployee();
+                case 1 -> {
+                    if (!showEmpresas(companies)) {
+                        System.out.println("\nNão existem empresas dentro da lista.");
+                    }
+                    break;
+                }
 
                 case 2 -> {
-                    int code = input.getIntegerNumber("Código do Empregado");
-                    company.employeeRecord(code);
+                    String nomeEmpresa = input.getText("\nEscolha um nome para a empresa");
+
+                    if (getEmpresa(companies, nomeEmpresa) == null) {
+                        companies.add(new Company(nomeEmpresa));
+                        System.out.println("A empresa " + nomeEmpresa + " foi adicionada com sucesso!");
+                    } else {
+                        System.out.println("Nome já utilizado!");
+                    }
+                    break;
                 }
 
                 case 3 -> {
-                    try {
-                        ArrayList<Employee> employees = company.getEmployeesFromFile();
-                        company.addMultipleEmployees(employees);
-                    } catch (IOException e) {
-                        System.out.println("Ocorreu um erro: " + e.getMessage());
+                    if (showEmpresas(companies)) {
+                        String nomeEmpresa = input.getText("Empresa que pretende remover");
+                        selectedCompany = getEmpresa(companies, nomeEmpresa);
+                        if (selectedCompany != null) {
+                            companies.remove(selectedCompany);
+                            System.out.println("A empresa " + nomeEmpresa + " foi removida com sucesso!");
+                        } else {
+                            System.out.println("Empresa não encontrada");
+                        }
+                    } else {
+                        System.out.println("\nNão existem empresas dentro da lista.");
                     }
+                    break;
                 }
 
                 case 4 -> {
-                    EmployeeCategory employeeCategory;
-                    String category = input.getText("Categoria");
+                    if (showEmpresas(companies)) {
+                        String nomeEmpresa = input.getText("\nEmpresa que pretende editar");
+                        selectedCompany = getEmpresa(companies, nomeEmpresa);
+                        if (selectedCompany != null) {
+                            //<editor-fold desc="Menu de gestão de uma empresa">
+                            do {
+                                System.out.println("*** Gestão de Recursos Humanos ***");
+                                System.out.println(selectedCompany + "\n");
+                                System.out.println("1. Adicionar Empregado");
+                                System.out.println("2. Pesquisar Empregado");
+                                System.out.println("3. Carregar Lista de Empregados");
+                                System.out.println("4. Número de Empregados de uma Categoria");
+                                System.out.println("5. Consultar Fichas de Empregados");
+                                System.out.println("6. Consultar Fichas de Empregados, filtrados por categoria");
+                                System.out.println("7. Total de Salários a Pagar");
+                                System.out.println("8. Custos Trimestrais, Semestrais e Anuais com Salários");
+                                System.out.println("9. Guardar Lista de Empregados");
+                                System.out.println("10. Alterar Informações da Empresa");
+                                System.out.println("0. Voltar ao menu");
 
-                    employeeCategory = switch (category.toUpperCase()) {
-                        case "GESTOR" ->
-                            EmployeeCategory.MANAGER;
-                        case "MOTORISTA" ->
-                            EmployeeCategory.DRIVER;
-                        case "COMERCIAL" ->
-                            EmployeeCategory.SALESMAN;
-                        default ->
-                            EmployeeCategory.NORMAL;
-                    };
+                                secOption = input.getIntegerNumber("Escolha uma opção");
 
-                    System.out.println("Nº de Empregados na Categoria: " + company.totalEmployeesInCategory(employeeCategory));
+                                switch (secOption) {
+                                    case 1 ->
+                                        selectedCompany.addEmployee();
+
+                                    case 2 -> {
+                                        int code = input.getIntegerNumber("Código do Empregado");
+                                        selectedCompany.employeeRecord(code);
+                                    }
+
+                                    case 3 -> {
+                                        try {
+                                            ArrayList<Employee> employees = selectedCompany.getEmployeesFromFile();
+                                            selectedCompany.addMultipleEmployees(employees);
+                                        } catch (IOException e) {
+                                            System.out.println("Ocorreu um erro: " + e.getMessage());
+                                        }
+                                    }
+
+                                    case 4 -> {
+                                        EmployeeCategory employeeCategory;
+                                        String category = input.getText("Categoria");
+
+                                        employeeCategory = switch (category.toUpperCase()) {
+                                            case "GESTOR" ->
+                                                EmployeeCategory.MANAGER;
+                                            case "MOTORISTA" ->
+                                                EmployeeCategory.DRIVER;
+                                            case "COMERCIAL" ->
+                                                EmployeeCategory.SALESMAN;
+                                            default ->
+                                                EmployeeCategory.NORMAL;
+                                        };
+
+                                        System.out.println("Nº de Empregados na Categoria: " + selectedCompany.totalEmployeesInCategory(employeeCategory));
+                                    }
+
+                                    case 5 ->
+                                        selectedCompany.employeeRecords();
+
+                                    case 6 ->
+                                        selectedCompany.employeeRecordsByCategory();
+
+                                    case 7 ->
+                                        selectedCompany.totalInSalaries();
+
+                                    case 8 ->
+                                        selectedCompany.showCosts();
+
+                                    case 9 ->
+                                        selectedCompany.writeEmployeesToFile();
+
+                                    case 10 -> {
+                                        String changeName = input.getText("Alterar nome da empresa (S/N)");
+
+                                        if (changeName.toUpperCase().equals("S")) {
+                                            selectedCompany.setName(input.getText("Novo nome"));
+                                        } else if (!changeName.toUpperCase().equals("N")) {
+                                            System.out.println("Resposta inválidada!");
+                                        }
+
+                                        String changeValues = input.getText("Alterar valores fixados (S/N)");
+                                        if (changeValues.toUpperCase().equals("S")) {
+                                            double workdayValue = input.getRealNumber("Valor por dia de trabalho");
+                                            double kilometerValue = input.getRealNumber("Valor por quilómetro");
+                                            double salesPercentage = input.getRealNumber("Percentagem de vendas");
+                                            selectedCompany.changeCompanyValues(workdayValue, kilometerValue, salesPercentage);
+                                        } else if (!changeName.toUpperCase().equals("N")) {
+                                            System.out.println("Resposta inválidada!");
+                                        }
+                                    }
+                                    case 0 -> {
+                                        System.out.println("Regressando ao menu...\n\n");
+                                        break;
+                                    }
+                                    default -> {
+                                        System.out.println("Opção inválida!");
+                                        break;
+                                    }
+                                }
+                            } while (secOption != 0);
+                        } //</editor-fold>}
+                        else {
+                            System.out.println("Empresa não encontrada");
+                        }
+                    } else {
+                        System.out.println("\nNão existem empresas dentro da lista.");
+                    }
+                    break;
                 }
 
-                case 5 ->
-                    company.employeeRecords();
-
-                case 6 ->
-                    company.employeeRecordsByCategory();
-
-                case 7 ->
-                    company.totalInSalaries();
-
-                case 8 ->
-                    company.showCosts();
-
-                case 9 ->
-                    company.writeEmployeesToFile();
-
-                case 10 -> {
-                    String changeName = input.getText("Alterar nome da empresa (S/N)");
-
-                    if (changeName.toUpperCase().equals("S")) {
-                        company.setName(input.getText("Novo nome"));
-                    } else if (!changeName.toUpperCase().equals("N")) {
-                        System.out.println("Resposta inválidada!");
-                    }
-
-                    String changeValues = input.getText("Alterar valores fixados (S/N)");
-                    if (changeValues.toUpperCase().equals("S")) {
-                        double workdayValue = input.getRealNumber("Valor por dia de trabalho");
-                        double kilometerValue = input.getRealNumber("Valor por quilómetro");
-                        double salesPercentage = input.getRealNumber("Percentagem de vendas");
-                        company.changeCompanyValues(workdayValue, kilometerValue, salesPercentage);
-                    } else if (!changeName.toUpperCase().equals("N")) {
-                        System.out.println("Resposta inválidada!");
-                    }
+                case 0 -> {
+                    System.out.println("Obrigado por usar o nosso programa!");
+                    break;
                 }
-
-                case 0 ->
-                    System.exit(0);
-
                 default -> {
                     System.out.println("Opção inválida!");
                     break;
